62e4e5812888ae4fe4e7a0d35052ae0827f931be
hackerton <9619080+HackerTon@users.noreply.github.com>
2021-02-17 15:23:35 +0800

    initial commit
    Signed-off-by: hackerton <9619080+HackerTon@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Plot from "react-plotly.js";
 
 function App() {
+  const [data, setData] = useState([0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Plot
+        data={[
+          {
+            type: "histogram",
+            x: data,
+            marker: {
+              color: "red",
+            },
+          },
+        ]}
+        layout={{ width: 1000, height: 500, title: "A Fancy Plot" }}
+      />
+      <button onClick={() => setData([...data, Math.random()])}>ABC</button>
     </div>
   );
 }
